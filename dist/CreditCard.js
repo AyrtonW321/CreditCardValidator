48d2834e0625d15f8f6b6c4e2d246f75f68e57a3
Quintrixyy <hquintrixyy@gmail.com>
2025-02-08 16:40:23 -0500

    Working CC generator and checker

--- dist/CreditCard.js
+++ dist/CreditCard.js
@@ -2,18 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function btnCheckCCNumber() {
     const inputElement = document.getElementById('txt-cc-number');
+    const outputParagraph = document.getElementById('info-paragraph');
+    outputParagraph.innerHTML = '';
     if (inputElement) {
-        const CC = inputElement.value;
-        console.log(CC);
+        const CC = inputElement.value.split('').map(Number);
+        if (CC.length == 16) {
+            checkCCNumber(CC);
+        }
+        else {
+            outputParagraph.innerHTML += 'Please enter a valid 16-digit credit card number.';
+        }
         return CC;
     }
     return null;
 }
+// THIS IS WORKING, DO NOT TOUCH
 function makeCCNumber() {
     let newCC = [];
     let CCTotal = 0;
-    const outputElement = document.getElementById('info-paragraph');
-    outputElement.innerHTML = '';
+    const outputParagraph = document.getElementById('info-paragraph');
+    outputParagraph.innerHTML = '';
     // generate the first 15 digets of the credit card number
     for (let i = 0; i < 15; i++) {
         newCC.push(Math.floor(Math.random() * 10));
@@ -24,12 +32,16 @@ function makeCCNumber() {
         // Luhn algorithm: double every second digit from the right
         if ((14 - i) % 2 === 0) {
             digit *= 2;
-            if (digit > 9)
+            if (digit > 9) {
                 digit -= 9;
+            }
+            CCTotal += digit;
         }
-        CCTotal += digit;
+        else {
+            CCTotal += digit;
+        }
     }
-    let lastDigit = (70 - CCTotal);
+    let lastDigit = (10 - (CCTotal % 10)) % 10;
     if (lastDigit < 0 || lastDigit > 9) {
         // Retry if number is negative or greater than 9
         return makeCCNumber();
@@ -38,13 +50,41 @@ function makeCCNumber() {
         newCC.push(lastDigit);
         CCTotal += lastDigit;
     }
-    if (CCTotal !== 70) {
-        // Retry if sum is not 70
+    if (CCTotal % 10 !== 0) {
         return makeCCNumber();
     }
     const newCCNumber = newCC.join('');
-    outputElement.innerHTML = 'Generated credit card number: ' + newCCNumber;
-    console.log(CCTotal);
-    console.log(newCC);
+    outputParagraph.innerHTML = 'Generated credit card number: ' + newCCNumber;
+    console.log('Generated credit card number: ' + newCCNumber);
+    console.log('Luhn sum:', CCTotal);
+    console.log('Generated CC:', newCC);
+}
+// BROKEN BELOW
+function checkCCNumber(ccNumber) {
+    const outputParagraph = document.getElementById('info-paragraph');
+    outputParagraph.innerHTML = '';
+    // placeholder js to see if CC number gets transfered correctly
+    console.log('Checking credit card number:', ccNumber);
+    let luhnSum = 0;
+    for (let i = 0; i < ccNumber.length; i++) {
+        let digit = ccNumber[i];
+        // Luhn algorithm: double every second digit from the right
+        if ((ccNumber.length - i) % 2 === 0) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+        luhnSum += digit;
+    }
+    console.log('Luhn sum:', luhnSum);
+    if (luhnSum % 10 === 0) {
+        return true;
+    }
+    else {
+        console.log('Invalid credit card number');
+        outputParagraph.innerHTML = 'Invalid credit card number';
+        return false;
+    }
 }
 //# sourceMappingURL=CreditCard.js.map
--- src/CreditCard.ts
+++ src/CreditCard.ts
@@ -1,20 +1,27 @@
+
 function btnCheckCCNumber() {
     const inputElement = document.getElementById('txt-cc-number') as HTMLInputElement;
+    const outputParagraph = document.getElementById('info-paragraph') as HTMLInputElement;
+    outputParagraph.innerHTML = '';
     if (inputElement) {
-        const CC = inputElement.value;
-        console.log(CC);
+        const CC: number[] = inputElement.value.split('').map(Number);
+        if (CC.length == 16){
+            checkCCNumber(CC);
+        } else {
+            outputParagraph.innerHTML += 'Please enter a valid 16-digit credit card number.';
+        }
         return CC;
     }
     return null;
 }
 
 
-// this dosent work i swear, idk how to check it so ill js make a check for it and work on this when I got the luhn alogrithm validator working
+// THIS IS WORKING, DO NOT TOUCH
 function makeCCNumber(){
     let newCC: number[] = [];
     let CCTotal: number = 0;
-    const outputElement = document.getElementById('info-paragraph') as HTMLInputElement;
-    outputElement.innerHTML = '';
+    const outputParagraph = document.getElementById('info-paragraph') as HTMLInputElement;
+    outputParagraph.innerHTML = '';
     // generate the first 15 digets of the credit card number
     for (let i = 0; i < 15; i++) {
         newCC.push(Math.floor(Math.random() * 10));
@@ -25,11 +32,15 @@ function makeCCNumber(){
         // Luhn algorithm: double every second digit from the right
         if ((14 - i) % 2 === 0) {
             digit *= 2;
-            if (digit > 9) digit -= 9;
+            if (digit > 9){
+                digit -= 9;
+            }
+            CCTotal += digit;
+        } else {
+            CCTotal += digit;
         }
-        CCTotal += digit;
     }
-    let lastDigit = (70 - CCTotal);
+    let lastDigit = (10 - (CCTotal % 10)) % 10;
     if (lastDigit < 0 || lastDigit > 9) {
         // Retry if number is negative or greater than 9
         return makeCCNumber();
@@ -37,12 +48,39 @@ function makeCCNumber(){
         newCC.push(lastDigit);
         CCTotal += lastDigit;
     }
-    if (CCTotal !== 70) {
-        // Retry if sum is not 70
+    if (CCTotal % 10 !==0){
         return makeCCNumber();
     }
     const newCCNumber = newCC.join('');
-    outputElement.innerHTML = 'Generated credit card number: ' + newCCNumber;
-    console.log(CCTotal);
-    console.log(newCC);
+    outputParagraph.innerHTML = 'Generated credit card number: ' + newCCNumber;
+    console.log('Generated credit card number: ' + newCCNumber);
+    console.log('Luhn sum:', CCTotal);
+    console.log('Generated CC:', newCC);
+}
+
+function checkCCNumber(ccNumber: number[]): boolean {
+    const outputParagraph = document.getElementById('info-paragraph') as HTMLInputElement;
+    outputParagraph.innerHTML = '';
+    // placeholder js to see if CC number gets transfered correctly
+    console.log('Checking credit card number:', ccNumber);
+    let luhnSum: number = 0;
+    for (let i = 0; i < ccNumber.length; i++) {
+        let digit: number = ccNumber[i];
+        // Luhn algorithm: double every second digit from the right
+        if ((ccNumber.length - i) % 2 === 0) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+        luhnSum += digit;
+    }
+    console.log('Luhn sum:', luhnSum);
+    if (luhnSum % 10 === 0) {
+        return true;
+    } else {
+        console.log('Invalid credit card number');
+        outputParagraph.innerHTML = 'Invalid credit card number';
+        return false;
+    }
 }
