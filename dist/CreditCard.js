90c9dc780c37eed4ab3a608b1b33772164e43eb3
Ayrton Wong <[ayrtonwongg@gmail.com]>
2025-02-09 03:14:55 -0500

    Changed some code. (comment late )

--- dist/CreditCard.js
+++ dist/CreditCard.js
@@ -2,30 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function btnCheckCCNumber() {
     // create a constant for accessing the credit card number input field
-    const inputElement = document.getElementById('txt-cc-number');
+    const inputElement = document.getElementById("txt-cc-number");
     // create a constant for accessing the paragraph for displaying results
-    const outputParagraph = document.getElementById('info-paragraph');
-    outputParagraph.innerHTML = '';
+    const outputParagraph = document.getElementById("info-paragraph");
+    outputParagraph.innerHTML = "";
     // check if the input field is not empty
     if (inputElement) {
         // create a constant for storing the credit card number as an array of numbers as it was a string previously
-        const CC = inputElement.value.split('').map(Number);
+        const CC = inputElement.value.split("").map(Number);
         // check if the credit card number is 16 digits long, if not, display an error message
         if (CC.length == 16) {
             checkCCNumber(CC);
             checkCCDetails(CC);
         }
         else {
-            outputParagraph.innerHTML += 'Please enter a valid 16-digit credit card number.';
+            outputParagraph.innerHTML +=
+                "Please enter a valid 16-digit credit card number.";
         }
     }
 }
 // Function to generate a new credit card number
 function checkCCNumber(ccNumber) {
     // create a constant for accessing the paragraph for displaying results
-    const outputParagraph = document.getElementById('info-paragraph');
+    const outputParagraph = document.getElementById("info-paragraph");
     // display the credit card number being checked in the console
-    console.log('Checking credit card number:', ccNumber);
+    console.log("Checking credit card number:", ccNumber);
     // create a variable to store the Luhn sum
     let luhnSum = 0;
     // calculate the Luhn sum
@@ -44,16 +45,16 @@ function checkCCNumber(ccNumber) {
         luhnSum += digit;
     }
     // display the Luhn sum in the console
-    console.log('Luhn sum:', luhnSum);
+    console.log("Luhn sum:", luhnSum);
     // check if the calculated Luhn sum is divisible by 10, if so, return true, else, return false and display an error message
     if (luhnSum % 10 === 0) {
-        console.log('Valid credit card number');
-        outputParagraph.innerHTML += 'Valid credit card number' + '<br>';
+        console.log("Valid credit card number");
+        outputParagraph.innerHTML += "Valid credit card number" + "<br>";
         return true;
     }
     else {
-        console.log('Invalid credit card number');
-        outputParagraph.innerHTML += 'Invalid credit card number' + '<br>';
+        console.log("Invalid credit card number");
+        outputParagraph.innerHTML += "Invalid credit card number" + "<br>";
         return false;
     }
 }
@@ -62,8 +63,8 @@ function makeCCNumber() {
     let newCC = []; // create an array to store the credit card number digits
     let LuhnSum = 0; // create a variable to store the Luhn sum
     // create a constant for accessing the paragraph for displaying results
-    const outputParagraph = document.getElementById('info-paragraph');
-    outputParagraph.innerHTML = '';
+    const outputParagraph = document.getElementById("card-number");
+    outputParagraph.innerHTML = "";
     // generate the first 15 digits of the credit card number
     for (let i = 0; i < 15; i++) {
         newCC.push(Math.floor(Math.random() * 10)); // generate a random number between 0 and 9 and add it to the array
@@ -92,7 +93,8 @@ function makeCCNumber() {
         // Retry if number is negative or greater than 9
         return makeCCNumber();
     }
-    else { // add the last digit to the credit card number
+    else {
+        // add the last digit to the credit card number
         newCC.push(lastDigit);
         LuhnSum += lastDigit;
     }
@@ -101,52 +103,91 @@ function makeCCNumber() {
         return makeCCNumber();
     }
     // Add hyphens every 4 digits and remove the trailing hyphen
-    const newCCNumber = newCC.join('').replace(/(\d{4})/g, '$1-').slice(0, -1);
+    const newCCNumber = newCC
+        .join("")
+        .replace(/(\d{4})/g, "$1-")
+        .slice(0, -1);
     outputParagraph.innerHTML = newCCNumber;
     // display the Luhn sum and the generated CC for debugging
-    console.log('Generated credit card number: ' + newCCNumber);
-    console.log('Luhn sum:', LuhnSum);
-    console.log('Generated CC:', newCC);
+    console.log("Generated credit card number: " + newCCNumber);
+    console.log("Luhn sum:", LuhnSum);
+    console.log("Generated CC:", newCC);
     return newCCNumber;
 }
 function checkCCDetails(ccNumber) {
-    const outputParagraph = document.getElementById('info-paragraph');
-    outputParagraph.innerHTML = 'Info about the credit card number:' + '<br>';
+    const outputParagraph = document.getElementById("info-paragraph");
+    const infoContainer = document.getElementById("info-container");
+    // Reset paragraph content
+    outputParagraph.innerHTML = "Info about the credit card number:" + "<br>";
     console.log(ccNumber);
-    let firstDigitInfo = ['Airlines', 'Airlines', 'Travel and Entertainment', 'Banking and Financial', 'Banking and Financial', 'Merchandising and Banking', 'Petroleum', 'Healthcare and Telecommunications', 'National Assignment'];
+    // Define industry categories based on the first digit
+    let firstDigitInfo = [
+        "Airlines",
+        "Airlines",
+        "Travel and Entertainment",
+        "Banking and Financial",
+        "Banking and Financial",
+        "Merchandising and Banking",
+        "Petroleum",
+        "Healthcare and Telecommunications",
+        "National Assignment",
+    ];
+    // Extract first digit to determine the issuing industry
     let firstDigit = ccNumber[0];
+    // Extract the first 6 digits for card type determination
     let first6Digits = ccNumber.slice(0, 6);
+    // Extract digits 7-15, which usually relate to the account number
     let first7To15Digits = ccNumber.slice(6, 15);
-    console.log(firstDigit);
-    console.log(first6Digits);
-    console.log(first7To15Digits);
-    // Check the first digit of the credit card number to determine the industry it was issued for
-    outputParagraph.innerHTML += 'This card was issued for the ' + firstDigitInfo[firstDigit - 1] + ' industry' + '<br>';
-    // Check the first 6 digits of the credit card number to determine the type of card
-    if (first6Digits[0] == 4) {
-        outputParagraph.innerHTML += 'This is a Visa card' + '<br>';
+    console.log("First digit:", firstDigit);
+    console.log("First 6 digits:", first6Digits);
+    console.log("Digits 7-15:", first7To15Digits);
+    // Display the industry information based on the first digit
+    outputParagraph.innerHTML +=
+        "This card was issued for the " +
+            firstDigitInfo[firstDigit - 1] +
+            " industry" +
+            "<br>";
+    // Determine card type based on first 6 digits
+    if (first6Digits[0] === 4) {
+        outputParagraph.innerHTML += "This is a Visa card" + "<br>";
     }
-    else if (first6Digits[0] == 5 && first6Digits[1] >= 1 && first6Digits[1] <= 5) {
-        outputParagraph.innerHTML += 'This is a MasterCard' + '<br>';
+    else if (first6Digits[0] === 5 &&
+        first6Digits[1] >= 1 &&
+        first6Digits[1] <= 5) {
+        outputParagraph.innerHTML += "This is a MasterCard" + "<br>";
     }
-    else if (first6Digits[0] == 3 && first6Digits[1] == 4 || first6Digits[1] == 7) {
-        outputParagraph.innerHTML += 'This is a American Express card' + '<br>';
+    else if (first6Digits[0] === 3 &&
+        (first6Digits[1] === 4 || first6Digits[1] === 7)) {
+        outputParagraph.innerHTML += "This is an American Express card" + "<br>";
     }
-    else if (first6Digits[0] == 6 && first6Digits[1] == 5) {
-        outputParagraph.innerHTML += 'This is a Discover card' + '<br>';
+    else if (first6Digits[0] === 6 && first6Digits[1] === 5) {
+        outputParagraph.innerHTML += "This is a Discover card" + "<br>";
     }
-    else if (first6Digits[0] == 6 && first6Digits[1] == 0 && first6Digits[2] == 1 && first6Digits[3] == 1) {
-        outputParagraph.innerHTML += 'This is a Discover card' + '<br>';
+    else if (first6Digits[0] === 6 &&
+        first6Digits[1] === 0 &&
+        first6Digits[2] === 1 &&
+        first6Digits[3] === 1) {
+        outputParagraph.innerHTML += "This is a Discover card" + "<br>";
     }
-    else if (first6Digits[0] == 6 && first6Digits[1] == 4 && first6Digits[2] == 4) {
-        outputParagraph.innerHTML += 'This is a Discover card' + '<br>';
+    else if (first6Digits[0] === 6 &&
+        first6Digits[1] === 4 &&
+        first6Digits[2] === 4) {
+        outputParagraph.innerHTML += "This is a Discover card" + "<br>";
     }
-    else if (first6Digits = [3, 7, 6, 2, 1, 1]) {
-        outputParagraph.innerHTML += 'This is a Signapore Airlines Krisflyer card' + '<br>';
+    else if (JSON.stringify(first6Digits) === JSON.stringify([3, 7, 6, 2, 1, 1])) {
+        outputParagraph.innerHTML +=
+            "This is a Singapore Airlines Krisflyer card" + "<br>";
     }
-    else if (first6Digits = [5, 2, 9, 9, 6, 2]) {
-        outputParagraph.innerHTML += 'This is a pre-paid Much-Music Mastercard' + '<br>';
+    else if (JSON.stringify(first6Digits) === JSON.stringify([5, 2, 9, 9, 6, 2])) {
+        outputParagraph.innerHTML +=
+            "This is a pre-paid MuchMusic Mastercard" + "<br>";
     }
-    outputParagraph.innerHTML += '7 digits regarding your bank account number: ' + first7To15Digits.join('') + '<br>';
+    // Display the digits related to the account number
+    outputParagraph.innerHTML +=
+        "7 digits regarding your bank account number: " +
+            first7To15Digits.join("") +
+            "<br>";
+    // Show the hidden info-container
+    infoContainer.style.display = "block";
 }
 //# sourceMappingURL=CreditCard.js.map
